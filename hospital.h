d91df1728fddb654ab9a6df931714f441380b5b3
subhashi18 <subhashiherath92@gmail.com>
2026-09-19 00:16:42 +0530

    add initial commit

---
+++ hospital.h
@@ -0,0 +1,4 @@
+#ifndef HOSPITAL_H
+#define HOSPITAL_H
+
+#endif
---
+++ main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "hospital.h"
+
+int main()
+{
+    printf("SMART HOSPITAL RESOURCE ALLOCATION\n");
+    return 0;
+}

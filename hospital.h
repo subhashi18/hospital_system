d99c6b3c9e9838ac0b6f85775a83dfd5b132a998
subhashi18 <subhashiherath92@gmail.com>
2026-09-19 00:25:49 +0530

    update constants for project

--- hospital.h
+++ hospital.h
@@ -1,4 +1,13 @@
 #ifndef HOSPITAL_H
 #define HOSPITAL_H
 
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_PATIENTS 100
+#define NUM_SPECIALTIES 4
+#define NUM_WARDS 4
+#define MAX_BEDS 20
+#define NAME_LENGTH 100
+
 #endif
